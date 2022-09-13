99451fb6df5c1109f266aeca8adbb1ce43a8db20
georgenardes <georgenardes@gmail.com>
2022-09-13 20:52:45 -0300

    corrigido erro de memória

--- src/connected_layer.c
+++ src/connected_layer.c
@@ -75,15 +75,21 @@ void backward_connected_layer(layer l, matrix prev_delta)
     // Then calculate dL/dw. Use axpy to subtract this dL/dw into any previously stored
     // updates for our weights, which are stored in l.dw
     // l.dw = l.dw - dL/dw
-    matrix dldw = matmul(transpose_matrix(in), delta);
+    matrix intransp = transpose_matrix(in);
+    matrix dldw = matmul(intransp, delta);
     axpy_matrix(-1.0, dldw, l.dw);
+    free_matrix(intransp);
 
     if (prev_delta.data) {
         // Finally, if there is a previous layer to calculate for,
         // calculate dL/d(in). Again, using axpy, add this into the current
         // value we have for the previous layers delta, prev_delta.
-        matrix w = matmul(delta, transpose_matrix(l.w));
+        matrix wtransp = transpose_matrix(l.w);
+        matrix w = matmul(delta, wtransp);
         axpy_matrix(1.0, w, prev_delta);
+        free_matrix(wtransp);
+        free_matrix(w);
+
     }
 }
 
